fa53f5d0c95c9bc2002b7ba0ae4be787a5cf3457
heh lol <heh lol>
2016-08-14 11:28:12 +0200

    Initial commit

---
+++ src/main/java/put/fc/sds/ps/ProjektSzkoleniowyApplication.java
@@ -0,0 +1,12 @@
+package put.fc.sds.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSzkoleniowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektSzkoleniowyApplication.class, args);
+	}
+}
---
+++ src/test/java/put/fc/sds/ps/ProjektSzkoleniowyApplicationTests.java
@@ -0,0 +1,16 @@
+package put.fc.sds.ps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjektSzkoleniowyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
